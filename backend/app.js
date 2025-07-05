d891faccbce68bc76c15a1325830ab009d225500
Deep Prajapati <dvprajapati7212@gmail.com>
2025-07-05 10:16:28 +0530

    Initial backend setup

---
+++ backend/app.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const path = require("path");
+const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
+const port = 3001;
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/roomster");
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Index Route
+
+app.get("/listings", async (req, res) => {
+  let allListing = await Listing.find({});
+  res.render("listing/index.ejs", { allListing });
+});
+
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listing/new.ejs");
+});
+// View Individual Post
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listing/show.ejs", { listing });
+});
+
+// Create Listing Route
+app.post("/listings", async (req, res) => {
+  // create new insta
+  const newListing = await new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+// Update Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listing/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings")
+})
+// app.get("/testListing", async (req, res) => {
+//   let sampleTest = new Listing({
+//     title: "Deep Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Mehsana, Gujarat",
+//     country: "India",
+//   });
+//   await sampleTest.save();
+//   console.log("Sample was saved");
+//   res.send("Successfully Saved");
+// });
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ backend/models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    // This default properties is for when image option not give to user and this default image will be used
+    default:
+      "https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+    // This set function of mongoose schema validatiors for geter and seter properties used to when we give option to image link to user and user forget to add image link then this image link will be used
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+// Listing Model
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
