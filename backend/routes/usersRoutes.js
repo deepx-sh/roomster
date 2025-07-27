9bc7107d7a0b6755e55a2563df74c5e32d225788
Deep Prajapati <dvprajapati7212@gmail.com>
2025-07-27 19:03:19 +0530

    feat: add post route in userRoutes to save information to db

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,8 +1,26 @@
-const express = require('express');
+const express = require("express");
+const wrapAsync = require("../utils/wrapAsync");
+const User = require("../models/user.js");
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-    res.render("users/signup.ejs")
+  res.render("users/signup.ejs");
 });
 
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = await new User({ email, username });
+      const registerUser = await User.register(newUser, password);
+      console.log(registerUser);
+      req.flash("successMsg", "Welcome to Roomsters!");
+      res.redirect("/listings");
+    } catch (error) {
+      req.flash("errorMsg", error.message);
+      res.redirect("/signup");
+    }
+  })
+);
 module.exports = router;
