398af128e3a98d8a631a89860a0eab8d5008a914
Deep Prajapati <dvprajapati7212@gmail.com>
2025-08-03 18:18:23 +0530

    feat: impplement image preview on edit page

--- backend/controllers/listings.js
+++ backend/controllers/listings.js
@@ -56,7 +56,9 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash("error", "Listing not found");
     return res.redirect("/listings");
   }
-  res.render("listings/edit.ejs", { listing });
+  let originalImageUrl = listing.image.url;
+  originalImageUrl.replace("/upload","/upload/h_50,w_100")
+  res.render("listings/edit.ejs", { listing,originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
