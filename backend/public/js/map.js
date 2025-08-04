2ffd9c0b77ea56055ba839b448ecf70d95dfc6ca
Deep Prajapati <dvprajapati7212@gmail.com>
2025-08-04 17:32:51 +0530

    feat: show location marker on map using stored geometry data

--- backend/public/js/map.js
+++ backend/public/js/map.js
@@ -1,9 +1,10 @@
-
+const [lng, lat] = coordinates;
   // Step 1: Initialize the map
-  const map = L.map('map').setView([23.0225, 72.5714], 6); // Set to India by default
+  const map = L.map('map').setView([lat,lng], 8); // Set to India by default
 
   // Step 2: Add OpenStreetMap tiles
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
   }).addTo(map);
 
+const marker = L.marker([lat,lng]).addTo(map);
